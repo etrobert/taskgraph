7ca7c7af0e1735af5d7be2831b3bd783f3631827
erobert <etiennerobert33@gmail.com>
2020-07-23 01:59:45 +0200

    wait for the dom to be loaded to setup cytoscape prevents bug where graph is drawn before font is loaded

--- index.js
+++ index.js
@@ -3,45 +3,45 @@
 // TODO change to a correct include
 import cytoscape from "./node_modules/cytoscape/dist/cytoscape.esm.min.js";
 
-const cy = cytoscape({
-  container: document.getElementById("cy"),
-  elements: [
-    {
-      data: { id: "a", name: "Arroser les plantes" },
-    },
-    {
-      data: { id: "b", name: "Remplir l'arrosoir" },
-    },
-    { data: { id: "ab", source: "a", target: "b" } },
-  ],
-  style: [
-    {
-      selector: "node",
-      style: {
-        width: "label",
-        height: "label",
-        "background-color": "rgb(139, 108, 68)",
-        padding: "1em",
-
-        label: "data(name)",
-        "font-family": "PermanentMarker",
-        "font-size": "1.5em",
-        "text-valign": "center",
-      },
-    },
-    {
-      selector: "edge",
-      style: {
-        "target-arrow-shape": "triangle",
-        "line-color": "black",
-        "target-arrow-color": "black",
-        "curve-style": "bezier",
-      },
-    },
-  ],
-});
-
 document.addEventListener("DOMContentLoaded", (event) => {
+  const cy = cytoscape({
+    container: document.getElementById("cy"),
+    elements: [
+      {
+        data: { id: "a", name: "Arroser les plantes" },
+      },
+      {
+        data: { id: "b", name: "Remplir l'arrosoir" },
+      },
+      { data: { id: "ab", source: "a", target: "b" } },
+    ],
+    style: [
+      {
+        selector: "node",
+        style: {
+          width: "label",
+          height: "label",
+          "background-color": "rgb(139, 108, 68)",
+          padding: "1em",
+
+          label: "data(name)",
+          "font-family": "PermanentMarker",
+          "font-size": "1.5em",
+          "text-valign": "center",
+        },
+      },
+      {
+        selector: "edge",
+        style: {
+          "target-arrow-shape": "triangle",
+          "line-color": "black",
+          "target-arrow-color": "black",
+          "curve-style": "bezier",
+        },
+      },
+    ],
+  });
+
   const newTask = document.getElementById("newTask");
   document.onkeyup = (event) => {
     if (event.key == "i") {
