a43fce36d00193715a7e4a368af453aa85eef51e
erobert <etiennerobert33@gmail.com>
2020-07-23 00:27:52 +0200

    improve graphics

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
+"use strict";
+
 // TODO change to a correct include
-import cytoscape from './node_modules/cytoscape/dist/cytoscape.esm.min.js'
+import cytoscape from "./node_modules/cytoscape/dist/cytoscape.esm.min.js";
 
 const cy = cytoscape({
-  container: document.getElementById('cy'),
-  elements: [ // list of graph elements to start with
-    { // node a
-      data: { id: 'a' }
+  container: document.getElementById("cy"),
+  elements: [
+    {
+      data: { id: "a", name: "Arroser les plantes" },
     },
-    { // node b
-      data: { id: 'b' }
+    {
+      data: { id: "b", name: "Remplir l'arrosoir" },
     },
-    { // edge ab
-      data: { id: 'ab', source: 'a', target: 'b' }
-    }
+    { data: { id: "ab", source: "a", target: "b" } },
   ],
   style: [
     {
-      selector: 'node',
+      selector: "node",
       style: {
-        'background-color': 'aquamarine',
-        'label': 'data(id)',
-        'text-valign': 'center'
-      }
+        width: "label",
+        height: "label",
+        "background-color": "rgb(139, 108, 68)",
+        padding: "1em",
+
+        label: "data(name)",
+        "font-family": "PermanentMarker",
+        "font-size": "1.5em",
+        "text-valign": "center",
+      },
     },
     {
-      selector: 'edge',
+      selector: "edge",
       style: {
-        'target-arrow-shape': 'triangle',
-        'line-color': 'black',
-        'target-arrow-color': 'black',
-        'curve-style': 'bezier'
-      }
-    }
-  ]
+        "target-arrow-shape": "triangle",
+        "line-color": "black",
+        "target-arrow-color": "black",
+        "curve-style": "bezier",
+      },
+    },
+  ],
 });
-
-console.log("Hello world!");
