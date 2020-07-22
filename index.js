11fb51704234a5fa6ed4eda0c934d0b7b97c03b0
erobert <etiennerobert33@gmail.com>
2020-07-23 01:56:21 +0200

    add new task input field

--- index.js
+++ index.js
@@ -40,3 +40,20 @@ const cy = cytoscape({
     },
   ],
 });
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const newTask = document.getElementById("newTask");
+  document.onkeyup = (event) => {
+    if (event.key == "i") {
+      newTask.style.display = "block";
+      newTask.focus();
+    }
+  };
+  newTask.onkeypress = (event) => {
+    if (event.key == "Enter") {
+      console.log("New task: " + newTask.value);
+      newTask.style.display = "none";
+      newTask.value = "";
+    }
+  };
+});
