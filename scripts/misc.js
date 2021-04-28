93fc122dacbbb39a36302f52f1eb3dcda543dbee
erobert <etiennerobert33@gmail.com>
2021-04-28 23:46:05 +0200

    Updates

--- scripts/misc.js
+++ scripts/misc.js
@@ -1,16 +1,19 @@
 export function querySelector(selectors) {
-  const element = document.querySelector(selectors);
-  if (!element)
-    throw Error(`No matching Element for "${selectors}"`);
-  return element;
+    const element = document.querySelector(selectors);
+    if (!element)
+        throw Error(`No matching Element for "${selectors}"`);
+    return element;
 }
 export function getElementById(id) {
-  const element = document.getElementById(id);
-  if (!element)
-    throw Error(`HTML Element ${id} was not found!`);
-  return element;
+    const element = document.getElementById(id);
+    if (!element)
+        throw Error(`HTML Element ${id} was not found!`);
+    return element;
 }
 export function removeFromArray(array, element) {
-  array.splice(array.indexOf(element), 1);
+    array.splice(array.indexOf(element), 1);
 }
+/**
+ * Snaps *value* to *target* if close to *target* by less than *offset*
+ */
 export const snap = (target) => (offset) => (value) => value > target - offset && value < target + offset ? target : value;
