d6c4eb983f4914c13e1568be78ffc9be3d1567c0
erobert <etiennerobert33@gmail.com>
2021-04-28 23:15:34 +0200

    Use snowpack to build

---
+++ snowpack.config.js
@@ -0,0 +1,14 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    "src/scripts": "/scripts",
+    "src/static": { url: "/", static: true, resolve: false },
+  },
+  plugins: ["@snowpack/plugin-typescript"],
+  buildOptions: {
+    out: "dist",
+  },
+};
