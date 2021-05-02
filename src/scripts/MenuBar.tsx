3935bf5a4e74bc2f708d22a8438098e1dd02bbcf
erobert <etiennerobert33@gmail.com>
2021-05-02 18:21:39 +0200

    Add rel="norefereere" to a tag

--- src/scripts/MenuBar.tsx
+++ src/scripts/MenuBar.tsx
@@ -43,7 +43,11 @@ const MenuBar = ({
       />
       <p>
         Made by
-        <a href="mailto:etiennerobert33@gmail.com" target="_blank">
+        <a
+          href="mailto:etiennerobert33@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
           Etienne Robert
         </a>
       </p>
