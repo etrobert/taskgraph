119791ce7cc2ced57c2df19605558feaf08c9ce9
erobert <etiennerobert33@gmail.com>
2021-05-03 14:33:12 +0200

    Reactor useKeyboardShortcuts following @justinline advice

--- src/scripts/useAppShortcuts.ts
+++ src/scripts/useAppShortcuts.ts
@@ -1,7 +1,7 @@
 import { loadFromFile, saveToFile, saveToLocalStorage } from ".";
 import { deleteSelected, selectAll } from "./graph";
 import { getElementById } from "./misc";
-import useKeyboardShortcut from "./useKeyboardShortcut";
+import useKeyboardShortcuts, { Shortcut } from "./useKeyboardShortcuts";
 
 const insertMode = () => {
   const newTask = getElementById("newTask");
@@ -10,32 +10,55 @@ const insertMode = () => {
 
 // TODO Conditionally activate or disable the shortcuts rather than having them test insert mode
 const useAppShortcuts = (): void => {
-  useKeyboardShortcut(["a"], (event) => {
-    if (insertMode()) return;
-    if (event.ctrlKey) selectAll();
-  });
+  const selectAllShortcut: Shortcut = {
+    keys: ["a"],
+    callback: (event) => {
+      if (insertMode()) return;
+      if (event.ctrlKey) selectAll();
+    },
+  };
 
-  useKeyboardShortcut(["i"], () => {
-    if (insertMode()) return;
-    const newTask = getElementById("newTask");
-    newTask.style.display = "block";
-    newTask.focus();
-  });
+  const insert = {
+    keys: ["i"],
+    callback: () => {
+      if (insertMode()) return;
+      const newTask = getElementById("newTask");
+      newTask.style.display = "block";
+      newTask.focus();
+    },
+  };
 
-  useKeyboardShortcut(["d", "Delete"], () => {
-    if (insertMode()) return;
-    deleteSelected();
-    saveToLocalStorage();
-  });
+  const deleteSelectedShortcut = {
+    keys: ["d", "Delete"],
+    callback: () => {
+      if (insertMode()) return;
+      deleteSelected();
+      saveToLocalStorage();
+    },
+  };
 
-  useKeyboardShortcut(["o"], (event) => {
-    if (insertMode()) return;
-    if (event.ctrlKey) loadFromFile();
-  });
+  const openFile: Shortcut = {
+    keys: ["o"],
+    callback: (event) => {
+      if (insertMode()) return;
+      if (event.ctrlKey) loadFromFile();
+    },
+  };
 
-  useKeyboardShortcut(["s"], (event) => {
-    if (insertMode()) return;
-    if (event.ctrlKey) saveToFile();
+  const saveFile: Shortcut = {
+    keys: ["s"],
+    callback: (event) => {
+      if (insertMode()) return;
+      if (event.ctrlKey) saveToFile();
+    },
+  };
+
+  useKeyboardShortcuts({
+    selectAll: selectAllShortcut,
+    insert,
+    delete: deleteSelectedShortcut,
+    openFile,
+    saveFile,
   });
 };
 
---
+++ src/scripts/useKeyboardShortcuts.ts
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+type Shortcut = {
+  keys: string[];
+  callback: (event: KeyboardEvent) => void;
+};
+
+type Shortcuts = Partial<Record<string, Shortcut>>;
+
+const useKeyboardShortcuts = (shortcuts: Shortcuts): void => {
+  const handler = (event: KeyboardEvent) => {
+    Object.values(shortcuts).forEach((shortcut) => {
+      if (!shortcut) return;
+      const { keys, callback } = shortcut;
+      if (keys.includes(event.key)) callback(event);
+    });
+  };
+  useEffect(() => {
+    document.addEventListener("keyup", handler);
+    return () => document.removeEventListener("keyup", handler);
+  });
+};
+
+export default useKeyboardShortcuts;
+
+export type { Shortcut };
