93be6f71a6b051fc06b49b217ebdb33b04295136
erobert <etiennerobert33@gmail.com>
2021-05-05 18:01:34 +0200

    Update tests to use noopProps

--- src/scripts/MenuBar.test.tsx
+++ src/scripts/MenuBar.test.tsx
@@ -5,49 +5,38 @@ import noop from "lodash/noop";
 
 import MenuBar from "./MenuBar";
 
+const noopProps = {
+  onClose: noop,
+  onLoad: noop,
+  onSave: noop,
+  onNewGraph: noop,
+};
+
 describe("MenuBar", () => {
   test("calls onLoad", () => {
     const onLoad = jest.fn();
-    render(
-      <MenuBar onClose={noop} onLoad={onLoad} onNewGraph={noop} onSave={noop} />
-    );
+    render(<MenuBar {...noopProps} onLoad={onLoad} />);
     userEvent.click(screen.getByText("Load"));
     expect(onLoad).toHaveBeenCalled();
   });
 
   test("calls onSave", () => {
     const onSave = jest.fn();
-    render(
-      <MenuBar onClose={noop} onLoad={noop} onNewGraph={noop} onSave={onSave} />
-    );
+    render(<MenuBar {...noopProps} onSave={onSave} />);
     userEvent.click(screen.getByText("Save"));
     expect(onSave).toHaveBeenCalled();
   });
 
   test("calls onNewGraph", () => {
     const onNewGraph = jest.fn();
-    render(
-      <MenuBar
-        onClose={noop}
-        onLoad={noop}
-        onNewGraph={onNewGraph}
-        onSave={noop}
-      />
-    );
+    render(<MenuBar {...noopProps} onNewGraph={onNewGraph} />);
     userEvent.click(screen.getByText("New Graph"));
     expect(onNewGraph).toHaveBeenCalled();
   });
 
   test("calls onClose", () => {
     const onClose = jest.fn();
-    render(
-      <MenuBar
-        onClose={onClose}
-        onLoad={noop}
-        onNewGraph={noop}
-        onSave={noop}
-      />
-    );
+    render(<MenuBar {...noopProps} onClose={onClose} />);
     userEvent.click(screen.getByLabelText("Close Menu"));
     expect(onClose).toHaveBeenCalled();
   });
