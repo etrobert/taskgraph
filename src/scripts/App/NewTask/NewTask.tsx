c8201f9107ab8db7c9426f8d2ce73df90ffb4bdc
erobert <etiennerobert33@gmail.com>
2021-05-14 00:32:33 +0200

    Use autofocus

--- src/scripts/App/NewTask/NewTask.tsx
+++ src/scripts/App/NewTask/NewTask.tsx
@@ -1,4 +1,4 @@
-import React, { KeyboardEventHandler, useEffect, useRef } from "react";
+import React, { KeyboardEventHandler, useRef } from "react";
 import { AddTask } from "@/graph";
 
 import "./NewTask.css";
@@ -10,8 +10,6 @@ type Props = {
 
 const NewTask = ({ onNewTask, onCancel }: Props): JSX.Element => {
   const ref = useRef<HTMLInputElement>(null);
-
-  useEffect(() => ref.current?.focus(), []);
 
   const onKeyPress: KeyboardEventHandler<HTMLInputElement> = (event) => {
     if (!ref.current) return;
@@ -28,6 +26,7 @@ const NewTask = ({ onNewTask, onCancel }: Props): JSX.Element => {
       onKeyPress={onKeyPress}
       onBlur={onCancel}
       ref={ref}
+      autoFocus
     />
   );
 };
