51758a3b90ee2dbd65fa4f18def348bdac986ce4
erobert <etiennerobert33@gmail.com>
2021-05-14 13:56:55 +0200

    Make the NewTaskInput more React

--- src/scripts/App/NewTaskInput/NewTaskInput.tsx
+++ src/scripts/App/NewTaskInput/NewTaskInput.tsx
@@ -1,4 +1,4 @@
-import React, { KeyboardEventHandler, useRef } from "react";
+import React, { useState } from "react";
 import { AddTask } from "@/graph";
 
 import "./NewTaskInput.css";
@@ -9,25 +9,26 @@ type Props = {
 };
 
 const NewTask = ({ onNewTask, onCancel }: Props): JSX.Element => {
-  const ref = useRef<HTMLInputElement>(null);
-
-  const onKeyPress: KeyboardEventHandler<HTMLInputElement> = (event) => {
-    if (!ref.current) return;
-    if (event.key === "Enter") {
-      if (ref.current.value) onNewTask({ name: ref.current.value });
-      else onCancel();
-    }
-  };
+  const [name, setName] = useState("");
 
   return (
-    <input
-      className="NewTask"
-      type="text"
-      onKeyPress={onKeyPress}
-      onBlur={onCancel}
-      ref={ref}
-      autoFocus
-    />
+    <form
+      onSubmit={(event) => {
+        // Prevent the page from reloading
+        event.preventDefault();
+        if (name) onNewTask({ name });
+        else onCancel();
+      }}
+    >
+      <input
+        className="NewTask"
+        type="text"
+        onChange={(event) => setName(event.target.value)}
+        onBlur={onCancel}
+        value={name}
+        autoFocus
+      />
+    </form>
   );
 };
 
