aa6d871f32b1ae8cc9e2938ac6e6f8ac4ab725d9
erobert <etiennerobert33@gmail.com>
2021-09-12 18:11:35 +0200

    Add more mock data

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -21,11 +21,29 @@ import NewTaskInput from "./NewTaskInput/NewTaskInput";
 
 import "./App.css";
 import { RecoilRoot } from "recoil";
-import { graphState } from "./atoms";
+import { dependencyStateFamily, graphState, taskStateFamily } from "./atoms";
 
 const graphMockState = {
-  tasks: ["task-1"],
-  dependencies: [],
+  tasks: ["task-1", "task-2"],
+  dependencies: ["dependency-1"],
+};
+
+const task1Mock = {
+  id: "task-1",
+  name: "task-1",
+  position: { x: 0, y: 0 },
+};
+
+const task2Mock = {
+  id: "task-2",
+  name: "task-2",
+  position: { x: 0, y: 0 },
+};
+
+const dependencyMock = {
+  id: "dependency-1",
+  predecessor: "task-1",
+  successor: "task-2",
 };
 
 const App = (): JSX.Element => {
@@ -57,7 +75,14 @@ const App = (): JSX.Element => {
   useAppShortcuts({ loadFromFile, insertMode, onDelete, onCreateTask });
 
   return (
-    <RecoilRoot initializeState={({ set }) => set(graphState, graphMockState)}>
+    <RecoilRoot
+      initializeState={({ set }) => {
+        set(graphState, graphMockState);
+        set(taskStateFamily("task-1"), task1Mock);
+        set(taskStateFamily("task-2"), task2Mock);
+        set(dependencyStateFamily("dependency-1"), dependencyMock);
+      }}
+    >
       <GraphInput
         onLoad={(graph) => {
           loadGraph(graph);
