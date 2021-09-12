287c3c3bc13b7a2eba8e62a02486bfb9f0953992
erobert <etiennerobert33@gmail.com>
2021-09-13 01:01:10 +0200

    Move the RecoilRoot in index

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -10,31 +10,6 @@ import GraphCanvas from "./GraphCanvas/GraphCanvas";
 import NewTaskInput from "./NewTaskInput/NewTaskInput";
 
 import "./App.css";
-import { RecoilRoot } from "recoil";
-import { dependencyStateFamily, graphState, taskStateFamily } from "./atoms";
-
-const graphMockState = {
-  tasks: ["task-1", "task-2"],
-  dependencies: ["dependency-1"],
-};
-
-const task1Mock = {
-  id: "task-1",
-  name: "task-1",
-  position: { x: 0, y: 0 },
-};
-
-const task2Mock = {
-  id: "task-2",
-  name: "task-2",
-  position: { x: 100, y: 100 },
-};
-
-const dependencyMock = {
-  id: "dependency-1",
-  predecessor: "task-1",
-  successor: "task-2",
-};
 
 const App = (): JSX.Element => {
   const [menuBarOpen, setMenuBarOpen] = useState(false);
@@ -54,14 +29,7 @@ const App = (): JSX.Element => {
   });
 
   return (
-    <RecoilRoot
-      initializeState={({ set }) => {
-        set(graphState, graphMockState);
-        set(taskStateFamily("task-1"), task1Mock);
-        set(taskStateFamily("task-2"), task2Mock);
-        set(dependencyStateFamily("dependency-1"), dependencyMock);
-      }}
-    >
+    <>
       <button
         className="App__menu-bar-open-button iconButton"
         onClick={() => setMenuBarOpen(true)}
@@ -97,7 +65,7 @@ const App = (): JSX.Element => {
           onCancel={() => setInsertMode(false)}
         />
       )}
-    </RecoilRoot>
+    </>
   );
 };
 
