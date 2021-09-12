78f06371af5dab09ef0681ba6c2e818d18c1dee1
erobert <etiennerobert33@gmail.com>
2021-09-13 01:16:32 +0200

    Update useGraphState

--- src/App/useGraphState.ts
+++ src/App/useGraphState.ts
@@ -1,18 +1,35 @@
 import { useRecoilCallback } from "recoil";
-import { graphState } from "./atoms";
+import { graphState, taskStateFamily } from "./atoms";
 
 type UseGraphState = () => {
+  addTask: (name: string) => void;
   clearGraph: () => void;
 };
 
+// TODO Use a real unique id generator
+const generateNewId = () => (Math.random() * 1000).toString();
+
 const useGraphState: UseGraphState = () => {
+  const addTask = useRecoilCallback(({ set }) =>
+    // TODO Allow to give more data than just name (position, status)
+    (name: string) => {
+      const id = generateNewId();
+      set(taskStateFamily(id), {
+        id,
+        name,
+        position: { x: 0, y: 0 },
+      });
+      set(graphState, (graph) => ({ ...graph, tasks: [...graph.tasks, id] }));
+    }
+  );
+
   const clearGraph = useRecoilCallback(
     ({ set }) =>
       () =>
         set(graphState, { tasks: [], dependencies: [] }),
     []
   );
-  return { clearGraph };
+  return { addTask, clearGraph };
 };
 
 export default useGraphState;
