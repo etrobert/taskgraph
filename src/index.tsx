389bc3b0ff01645f192902d0e18c9932517e9005
erobert <etiennerobert33@gmail.com>
2021-09-13 01:01:10 +0200

    Move the RecoilRoot in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
 
 import App from "./App/App";
+import {
+  dependencyStateFamily,
+  graphState,
+  taskStateFamily,
+} from "./App/atoms";
+
+const graphMockState = {
+  tasks: ["task-1", "task-2"],
+  dependencies: ["dependency-1"],
+};
+
+const task1Mock = {
+  id: "task-1",
+  name: "task-1",
+  position: { x: 0, y: 0 },
+};
+
+const task2Mock = {
+  id: "task-2",
+  name: "task-2",
+  position: { x: 100, y: 100 },
+};
+
+const dependencyMock = {
+  id: "dependency-1",
+  predecessor: "task-1",
+  successor: "task-2",
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
+  ReactDOM.render(
+    <RecoilRoot
+      initializeState={({ set }) => {
+        set(graphState, graphMockState);
+        set(taskStateFamily("task-1"), task1Mock);
+        set(taskStateFamily("task-2"), task2Mock);
+        set(dependencyStateFamily("dependency-1"), dependencyMock);
+      }}
+    >
+      <App />
+    </RecoilRoot>,
+    document.getElementById("root")
+  );
 });
