cb03fb79fbc30eecda5d0b49266ea7466ec46933
erobert <etiennerobert33@gmail.com>
2021-09-18 02:45:51 +0200

    WIP

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -10,6 +10,79 @@ import NewTaskInput from "./NewTaskInput/NewTaskInput";
 import "./App.css";
 import useGraphState from "./useGraphState";
 
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3Le2ZBbGHEzjgk3yCNxh3Ioj8iNIC-EY",
+  authDomain: "taskgraph-e9cf9.firebaseapp.com",
+  databaseURL:
+    "https://taskgraph-e9cf9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "taskgraph-e9cf9",
+  storageBucket: "taskgraph-e9cf9.appspot.com",
+  messagingSenderId: "289117132023",
+  appId: "1:289117132023:web:10e62360aa751864e06ff2",
+  measurementId: "G-DKBYJ7EZBY",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+
+// ARCHITECTURE DECISION:
+// Use a collection projects
+// Each document contains a Map of Arrays of Maps like so:
+// {
+//   tasks: [{
+//     id: '',
+//     name: '',
+//     position: {
+//       x: 0,
+//       y: 0,
+//     }
+//   }],
+//   dependencies: []
+// }
+// When the size of a project will grow above 1MB it will be time to change
+
+const mockTask = {
+  id: "id-123",
+  name: "hello",
+  status: "ready",
+  position: {
+    x: 2,
+    y: 2,
+  },
+};
+
+const addProject = async () => {
+  const ref = collection(firestore, "projects");
+  await addDoc(ref, {
+    tasks: [mockTask, mockTask],
+    dependencies: [],
+  });
+};
+
+const useSyncFirestore = (id: string) => {
+  const ref = doc(firestore, "projects", id);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(ref, (snapshot) =>
+      console.log(snapshot.data())
+    );
+    return unsubscribe;
+  });
+};
+
+// addProject();
+
 const App = (): JSX.Element => {
   const [menuBarOpen, setMenuBarOpen] = useState(false);
   const closeMenuBar = () => setMenuBarOpen(false);
@@ -30,6 +103,8 @@ const App = (): JSX.Element => {
     onDelete: () => {},
     onCreateTask,
   });
+
+  useSyncFirestore("spXxYVulTgfKcj0n1sWb");
 
   return (
     <>
