866781845b020c86a8a343969d11c44a2d06f46d
erobert <etiennerobert33@gmail.com>
2021-09-19 17:00:23 +0200

    Remove outdated comment

--- src/App/useGraphState.ts
+++ src/App/useGraphState.ts
@@ -12,7 +12,6 @@ type UseGraphState = () => {
 const useGraphState: UseGraphState = () => {
   const addTask = useRecoilCallback(
     ({ set }) =>
-      // TODO Allow to give more data than just name (position, status)
       (task: Task) => {
         set(taskStateFamily(task.id), task);
         set(projectState, (project) => ({
