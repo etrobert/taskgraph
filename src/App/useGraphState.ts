b86a96c793264edf29fb530e613ff02caaabee00
erobert <etiennerobert33@gmail.com>
2021-09-19 16:40:03 +0200

    Fix task ids

--- src/App/useGraphState.ts
+++ src/App/useGraphState.ts
@@ -7,19 +7,15 @@ type UseGraphState = () => {
   clearGraph: () => void;
 };
 
-// TODO Use a real unique id generator
-const generateNewId = () => (Math.random() * 1000).toString();
-
 const useGraphState: UseGraphState = () => {
   const addTask = useRecoilCallback(
     ({ set }) =>
       // TODO Allow to give more data than just name (position, status)
       (task: Task) => {
-        const id = generateNewId();
-        set(taskStateFamily(id), task);
+        set(taskStateFamily(task.id), task);
         set(projectState, (project) => ({
           ...project,
-          tasks: [...project.tasks, id],
+          tasks: [...project.tasks, task.id],
         }));
       },
     []
