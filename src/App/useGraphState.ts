1ccdf468ab7301d0ff008280c12498c091863128
erobert <etiennerobert33@gmail.com>
2021-09-19 16:11:51 +0200

    Fix useGraphState dependency array

--- src/App/useGraphState.ts
+++ src/App/useGraphState.ts
@@ -11,20 +11,22 @@ type UseGraphState = () => {
 const generateNewId = () => (Math.random() * 1000).toString();
 
 const useGraphState: UseGraphState = () => {
-  const addTask = useRecoilCallback(({ set }) =>
-    // TODO Allow to give more data than just name (position, status)
-    (name: string) => {
-      const id = generateNewId();
-      set(taskStateFamily(id), {
-        name,
-        position: { x: 0, y: 0 },
-        status: "ready",
-      } as const);
-      set(projectState, (project) => ({
-        ...project,
-        tasks: [...project.tasks, id],
-      }));
-    }
+  const addTask = useRecoilCallback(
+    ({ set }) =>
+      // TODO Allow to give more data than just name (position, status)
+      (name: string) => {
+        const id = generateNewId();
+        set(taskStateFamily(id), {
+          name,
+          position: { x: 0, y: 0 },
+          status: "ready",
+        } as const);
+        set(projectState, (project) => ({
+          ...project,
+          tasks: [...project.tasks, id],
+        }));
+      },
+    []
   );
 
   const clearGraph = useRecoilCallback(
@@ -33,6 +35,7 @@ const useGraphState: UseGraphState = () => {
         set(projectState, { tasks: [], dependencies: [] }),
     []
   );
+
   return { addTask, clearGraph };
 };
 
