8e9eb3faa356d76f85f476de3603c61b96bea849
erobert <etiennerobert33@gmail.com>
2021-09-19 15:19:44 +0200

    Extract the firestore config

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -10,48 +10,9 @@ import NewTaskInput from "./NewTaskInput/NewTaskInput";
 import "./App.css";
 import useGraphState from "./useGraphState";
 
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  doc,
-  onSnapshot,
-} from "firebase/firestore";
+import { collection, addDoc, doc, onSnapshot } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC3Le2ZBbGHEzjgk3yCNxh3Ioj8iNIC-EY",
-  authDomain: "taskgraph-e9cf9.firebaseapp.com",
-  databaseURL:
-    "https://taskgraph-e9cf9-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "taskgraph-e9cf9",
-  storageBucket: "taskgraph-e9cf9.appspot.com",
-  messagingSenderId: "289117132023",
-  appId: "1:289117132023:web:10e62360aa751864e06ff2",
-  measurementId: "G-DKBYJ7EZBY",
-};
-
-const app = initializeApp(firebaseConfig);
-
-const firestore = getFirestore(app);
-
-// ARCHITECTURE DECISION:
-// Use a collection projects
-// Each document contains a Map of Arrays of Maps like so:
-// {
-//   tasks: [{
-//     id: '',
-//     name: '',
-//     position: {
-//       x: 0,
-//       y: 0,
-//     }
-//   }],
-//   dependencies: []
-// }
-// When the size of a project will grow above 1MB it will be time to change
+import firestore from "@/firestore";
 
 const mockTask = {
   id: "id-123",
