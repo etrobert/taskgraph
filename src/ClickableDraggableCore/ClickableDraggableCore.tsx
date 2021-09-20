94afc599224d62aae8817b563e9c13c1c6d5c124
erobert <etiennerobert33@gmail.com>
2021-09-20 19:58:26 +0200

    Test ClickableDraggableCore without using the offset

--- src/ClickableDraggableCore/ClickableDraggableCore.tsx
+++ src/ClickableDraggableCore/ClickableDraggableCore.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import {
   DraggableCore,
   DraggableCoreProps,
@@ -24,24 +24,32 @@ const ClickableDraggableCore = ({
   children,
   onStart,
   onStop,
+  onDrag,
   onClick,
   threshold = 5,
   ...props
 }: Props): JSX.Element => {
-  const [dragOrigin, setDragOrigin] = useState<Point>({ x: 0, y: 0 });
+  // const [dragOrigin, setDragOrigin] = useState<Point>({ x: 0, y: 0 });
+  const [dragging, setDragging] = useState<boolean>(false);
+
+  useEffect(() => console.log(dragging), [dragging]);
 
   return (
     <DraggableCore
       onStart={(event, data) => {
-        setDragOrigin(data);
         onStart?.(event, data);
+      }}
+      onDrag={(event, data) => {
+        console.log("onDrag");
+        setDragging(true);
+        onDrag?.(event, data);
       }}
       onStop={(event, data) => {
         // If it has not moved a lot since the beginning of the drag
         // We consider it as a click
-        if (squaredDistance(dragOrigin, data) <= threshold * threshold)
-          onClick(event, data);
+        if (!dragging) onClick(event, data);
         onStop?.(event, data);
+        setDragging(false);
       }}
       {...props}
     >
