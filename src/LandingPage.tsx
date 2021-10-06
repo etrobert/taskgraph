1d87d0c95d2df6cde2e66b1f0220a0549d58e34d
erobert <etiennerobert33@gmail.com>
2021-10-06 12:58:49 +0200

    Add the subscribe form

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -3,10 +3,6 @@ import React from "react";
 import "./LandingPage.css";
 
 const LandingPage = (): JSX.Element => {
-  // TODO: Use the real CTA
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const onCtaClick = () => {};
-
   return (
     <div className="LandingPage">
       <h1 className="LandingPage__title">
@@ -14,9 +10,47 @@ const LandingPage = (): JSX.Element => {
         <br />
         Graph
       </h1>
-      <button className={"LandingPage__cta"} onClick={onCtaClick}>
-        START NOW
-      </button>
+
+      <form
+        action="https://gmail.us5.list-manage.com/subscribe/post?u=fcf0ac58a5bd4d1fe9162f855&amp;id=389c9dac43"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        target="_blank"
+        className="LandingPage__form"
+        noValidate
+      >
+        <label htmlFor="mce-EMAIL">
+          <h2>Get our Latest Updates</h2>
+        </label>
+        <input
+          type="email"
+          name="EMAIL"
+          className="LandingPage__email-input"
+          id="mce-EMAIL"
+          placeholder="email address"
+          required
+        />
+        {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
+        <div
+          style={{ position: "absolute", left: "-5000px" }}
+          aria-hidden="true"
+        >
+          <input
+            type="text"
+            name="b_fcf0ac58a5bd4d1fe9162f855_389c9dac43"
+            tabIndex={-1}
+            value=""
+          />
+        </div>
+        <input
+          type="submit"
+          value="Subscribe"
+          name="subscribe"
+          id="mc-embedded-subscribe"
+          className="LandingPage__cta"
+        />
+      </form>
     </div>
   );
 };
