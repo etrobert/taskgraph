abdaf9ea9084407768a1941c6a3c738e5e86d183
erobert <etiennerobert33@gmail.com>
2021-10-06 14:43:25 +0200

    Updates

--- dist/App/App.js
+++ dist/App/App.js
@@ -1,7 +1,12 @@
 import LandingPage from "../LandingPage.js";
 import React from "../../snowpack/pkg/react.js";
+import {BrowserRouter as Router, Route, Switch} from "../../snowpack/pkg/react-router-dom.js";
 import "./App.css.proxy.js";
 const App = () => {
-  return /* @__PURE__ */ React.createElement(LandingPage, null);
+  return /* @__PURE__ */ React.createElement(Router, null, /* @__PURE__ */ React.createElement(Switch, null, /* @__PURE__ */ React.createElement(Route, {
+    path: "/graph"
+  }, "Graph"), /* @__PURE__ */ React.createElement(Route, {
+    path: "/"
+  }, /* @__PURE__ */ React.createElement(LandingPage, null), ";")));
 };
 export default App;
