45a73491d6952d421b0d20f3747000860f4f7ed1
erobert <etiennerobert33@gmail.com>
2021-10-08 16:42:54 +0200

    Remove extraneous semicolon

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -14,7 +14,7 @@ const App = (): JSX.Element => {
           <GraphPage />
         </Route>
         <Route path="/">
-          <LandingPage />;
+          <LandingPage />
         </Route>
       </Switch>
     </Router>
