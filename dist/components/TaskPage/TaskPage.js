942ceec6b324586123fc0051ca5612fce299d544
erobert <etiennerobert33@gmail.com>
2021-10-23 10:00:38 +0200

    Updates

--- dist/components/TaskPage/TaskPage.js
+++ dist/components/TaskPage/TaskPage.js
@@ -2,16 +2,17 @@ import React from "../../../_snowpack/pkg/react.js";
 import {useRecoilValue} from "../../../_snowpack/pkg/recoil.js";
 import {Link} from "../../../_snowpack/pkg/react-router-dom.js";
 import {deleteField} from "../../../_snowpack/pkg/@firebase/firestore.js";
-import {taskStateFamily} from "../../atoms.js";
+import {taskIsInWorkspaceStateFamily, taskStateFamily} from "../../atoms.js";
 import useFirestoreState from "../../hooks/useFirestoreState.js";
 import "./TaskPage.css.proxy.js";
 const TaskPage = ({match}) => {
   const {id} = match.params;
+  const taskIsInWorkspace = useRecoilValue(taskIsInWorkspaceStateFamily(id));
   const {name, priority} = useRecoilValue(taskStateFamily(id));
   const {updateTask} = useFirestoreState();
   return /* @__PURE__ */ React.createElement("div", {
     className: "TaskPage"
-  }, /* @__PURE__ */ React.createElement("h1", null, name), /* @__PURE__ */ React.createElement("label", null, "Priority:", " ", /* @__PURE__ */ React.createElement("select", {
+  }, /* @__PURE__ */ React.createElement("h1", null, taskIsInWorkspace ? name : "Task not found"), taskIsInWorkspace && /* @__PURE__ */ React.createElement("label", null, "Priority:", " ", /* @__PURE__ */ React.createElement("select", {
     value: priority ?? "",
     onChange: (event) => {
       const value = event.target.value;
