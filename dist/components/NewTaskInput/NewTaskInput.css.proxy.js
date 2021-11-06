c53a6e01151fec23ef7a58eba086eea95a758197
erobert <etiennerobert33@gmail.com>
2021-11-06 14:58:07 +0100

    Updates

--- dist/components/NewTaskInput/NewTaskInput.css.proxy.js
+++ dist/components/NewTaskInput/NewTaskInput.css.proxy.js
@@ -1,6 +1,6 @@
 // [snowpack] add styles to the page (skip if no document exists)
 if (typeof document !== 'undefined') {
-  const code = ".NewTask {\n  position: absolute;\n  left: 0;\n  right: 0;\n  height: 1.5em;\n  top: calc(50% - (1.5em / 2));\n  margin: auto;\n\n  z-index: 1;\n\n  width: 80vw;\n  padding: 1em;\n\n  opacity: 80%;\n\n  font-family: \"PermanentMarker\";\n  font-size: 1.5em;\n}\n";
+  const code = ".NewTask {\n  position: absolute;\n  left: 0;\n  right: 0;\n  height: 1.5em;\n  top: calc(50% - (1.5em / 2));\n  margin: auto;\n\n  z-index: 1;\n\n  width: 80vw;\n  padding: 1em;\n\n  opacity: 80%;\n\n  font-size: 1.5em;\n  font-weight: bold;\n}\n";
 
   const styleEl = document.createElement("style");
   const codeEl = document.createTextNode(code);
